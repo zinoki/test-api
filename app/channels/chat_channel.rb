48d8673384f019d79bfaf5527c5f8315ac4e9546
Zachary Inouye <zinouye@gmail.com>
2018-08-16 16:56:19 -0700

    set up rebroadcasting

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -4,4 +4,8 @@ class ChatChannel < ApplicationCable::Channel
   def subscribed
     stream_from "chat_#{params[:room]}"
   end
+
+  def receive(data)
+    ActionCable.server.broadcast("chat_#{params[:room]}", data)
+  end
 end
